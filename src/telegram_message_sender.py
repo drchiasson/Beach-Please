529cc5f5d16fe3ebb0df3a8a9d6ab94c039494b2
drchiasson <dyami.chiasson@gmail.com>
2026-07-10 17:03:02 -0700

    Added telegram message sender tool and .DS_Store to git ignore

---
+++ src/telegram_message_sender.py
@@ -0,0 +1,12 @@
+import os
+import asyncio
+from telegram import Update, ForceReply
+from telegram.ext import CallbackContext, Application, MessageHandler, filters, ContextTypes
+
+async def send_message_to_channel(application, message):
+    await application.bot.send_message(os.environ.get("TELEGRAM_CHANNEL_KEY"), message)
+
+
+def send_bot_message(message) -> None:
+    application = Application.builder().token(os.environ.get("TELEGRAM_BOT_KEY")).build()
+    asyncio.run(send_message_to_channel(application, message))
